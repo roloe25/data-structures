c80e8b3d9741d9c3b3267a1f048ae15e09c1e87a
Albert Jo <for.aljo@gmail.com>
2020-02-06 16:38:22 -0800

    es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,30 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
   constructor() {
+    this.storage = {};
+    this.nextOut = 0;
+    this.last = 0;
+  }
+  dequeue() {
+    if (Object.keys(this.storage).length === 0) {
+      return 0;
+    }
+    this.nextOut++;
+    var next = this.storage[this.nextOut];
+    delete this.storage[this.nextOut];
+    return next;
+  }
+
+  enqueue(string) {
+    this.last++;
+    this.storage[this.last] = string;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
+
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.curr = 0;
+    this.storage = {};
+  }
+
+  push(string) {
+    this.curr++;
+    this.storage[this.curr] = string;
+  }
+
+  pop() {
+    if (Object.keys(this.storage).length === 0) {
+      return 0;
+    }
+    var popped = this.storage[this.curr];
+    delete this.storage[this.curr];
+    this.curr--;
+    return popped;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
