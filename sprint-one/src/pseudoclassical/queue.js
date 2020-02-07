ae987ddade6aa96836135f9c94bbd871b24ca670
Albert Jo <for.aljo@gmail.com>
2020-02-06 16:37:29 -0800

    psuedo

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.nextOut = 0;
+  this.last = 0;
+};
+
+
+Queue.prototype.dequeue = function () {
+  if (Object.keys(this.storage).length === 0) {
+    return 0;
+  }
+  this.nextOut++;
+  var next = this.storage[this.nextOut];
+  delete this.storage[this.nextOut];
+  return next;
+};
+
+Queue.prototype.enqueue = function (string) {
+  this.last++;
+  this.storage[this.last] = string;
+};
+
+Queue.prototype.size = function () {
+  return Object.keys(this.storage).length;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.curr = 0;
+  this.storage = {};
+};
+
+Stack.prototype.push = function(string) {
+  this.curr++;
+  this.storage[this.curr] = string;
+};
+
+Stack.prototype.pop = function () {
+  if (Object.keys(this.storage).length === 0) {
+    return 0;
+  }
+  var popped = this.storage[this.curr];
+  delete this.storage[this.curr];
+  this.curr--;
+  return popped;
+};
+
+Stack.prototype.size = function () {
+  return Object.keys(this.storage).length;
 };
 
 
