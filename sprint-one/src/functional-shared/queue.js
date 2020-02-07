61ce0cc219456e8663cfd5d89de00530bd1361c8
Albert Jo <for.aljo@gmail.com>
2020-02-06 16:35:43 -0800

    functional shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue.storage = {};
+  queue.nextOut = 0;
+  queue.last = 0;
+
+  _.extend(queue, queueMethods);
+
+
+  return queue;
 };
 
 var queueMethods = {};
 
+
+queueMethods.enqueue = function (string) {
+  this.last++;
+  this.storage[this.last] = string;
+};
+
+queueMethods.dequeue = function () {
+  this.nextOut++;
+  var next = this.storage[this.nextOut];
+  delete this.storage[this.nextOut];
+  return next;
+};
+
+queueMethods.size = function () {
+  return Object.keys(this.storage).length;
+};
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,43 @@
+//made syntax error why does this not work
+//for a quiz questions
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.currIndex = 0;
+  stack.stackStorage = {};
+  _.extend(stack, stackMethods);
+  return stack;
 };
 
 var stackMethods = {};
+
+
+
+stackMethods.push = function(string) {
+  this.currIndex++;
+  this.stackStorage[this.currIndex] = string;
+};
+
+stackMethods.pop = function() {
+  if (Object.keys(this.stackStorage).length === 0) {
+    return 0;
+  } else {
+    var popped = this.stackStorage[this.currIndex];
+    delete this.stackStorage[this.currIndex];
+    this.currIndex--;
+    return popped;
+  }
+};
+
+stackMethods.size = function(string) {
+  return Object.keys(this.stackStorage).length;
+};
+
+//stackMethods.stackStorage = {};
+
+
+
 
 
