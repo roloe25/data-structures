78d08282ae228af36142b56b562c89ce150cdd05
Albert Jo <for.aljo@gmail.com>
2020-02-09 12:43:47 -0800

    linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,57 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    if (list.tail === null) {
+      list.tail = newNode;
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var headValue = list.head.value;
+    list.head = list.head.next;
+    return headValue;
+
   };
 
   list.contains = function(target) {
+    /*
+    var isfound = false
+    function finding() {
+
+   if the obj's "value" is the target
+    isfound = true
+    else
+      if the obj's "next" value is null
+        return
+      if the obj's "next" value is an object
+        repeat this process with the obj's "next value"
+    }
+
+    return isfound
+    */
+    var isFound = false;
+    var copyObj = Object.assign({}, list.head);
+
+    var findingFn = function(nodeChain) {
+      if (nodeChain.value === target) {
+        isFound = true;
+        return;
+      } else {
+        if (nodeChain.next === null) {
+          return;
+        } else {
+          findingFn(nodeChain.next);
+        }
+      }
+    };
+
+    findingFn(copyObj);
+    return isFound;
   };
 
   return list;
